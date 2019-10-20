352a21d7311523a38c149d13881fd387d7c84351
Patrick Nolin <patrick.nolin95@gmail.com>
2019-10-20 19:01:55 -0400

    Basic layout and style for schedule.
    - Score of finished and live games.
    - Record of teams.

--- src/components/games-list.component.tsx
+++ src/components/games-list.component.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Game as GameModel } from "../models/schedule.model";
 import Game from "./game.component";
+import "../styles/games-list.css";
 
 interface Props {
   games: GameModel[];
@@ -11,7 +12,7 @@ const GamesList: React.FC<Props> = (games: Props) => {
     <Game key={game.gamePk} game={game}></Game>
   ));
 
-  return <ul>{gamesComponents}</ul>;
+  return <ul id="game-list">{gamesComponents}</ul>;
 };
 
 export default GamesList;
