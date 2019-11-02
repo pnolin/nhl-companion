e61ca64c64dc81168b1d9d4b435a1b1fc0e3b8fa
Patrick Nolin <patrick.nolin95@gmail.com>
2019-11-02 19:57:20 -0400

    Packaging the app for windows.
    - Updated the startUrl when not in dev mode.
    - Updated information in package.json.
    - Installed electron-package module.
    - Updated .gitignore to ignore out folder.

--- electron-starter.js
+++ electron-starter.js
@@ -25,7 +25,7 @@ function createWindow() {
   const startUrl =
     process.env.ELECTRON_START_URL ||
     url.format({
-      pathname: path.join(__dirname, "/../build/index.html"),
+      pathname: path.join(__dirname, "./public/build/index.html"),
       protocol: "file:",
       slashes: true
     });
