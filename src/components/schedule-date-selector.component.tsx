ce4a10e364d95dcddd322a79027381872c946b13
Patrick Nolin <patrick.nolin95@gmail.com>
2020-02-17 21:31:01 -0500

    Added left/right arrows next to the date.
    - Added fort awesome packages.

--- src/components/schedule-date-selector.component.tsx
+++ src/components/schedule-date-selector.component.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import moment from "moment";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import "../styles/schedule-date-selector.css";
 
@@ -10,9 +11,11 @@ interface Props {
 const ScheduleDateSelector: React.FC<Props> = (props: Props) => {
   return (
     <div id="schedule-date-selector">
+      <FontAwesomeIcon icon="angle-left" />
       <span className="body-text">
         {moment(props.date).format("DD/MM/YYYY")}
       </span>
+      <FontAwesomeIcon icon="angle-right" />
     </div>
   );
 };
